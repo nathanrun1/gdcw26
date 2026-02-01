a752cf0c125d9b24e49cfbce43d96ecde8808412
Parth Chaturvedi <parthcv0101@gmail.com>
2026-02-01 11:33:26 -0800

    Add main menu

---
+++ Assets/Scripts/Game/BlinkingText.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game
+{
+    public class BlinkingText : MonoBehaviour
+    {
+        [SerializeField] private float _blinkSpeed = 1f;
+        private TextMeshProUGUI _text;
+        private float _timer;
+    
+        private void Start()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+    
+        private void Update()
+        {
+            _timer += Time.deltaTime * _blinkSpeed;
+            Color color = _text.color;
+            color.a = Mathf.PingPong(_timer, 1f);
+            _text.color = color;
+        }
+    }
+}
