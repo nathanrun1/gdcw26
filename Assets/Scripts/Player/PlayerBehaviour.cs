48bbc6b709f26fa52299cbae020ef2f0af31dfd7
nathanrun1 <nathanmreilly@gmail.com>
2026-01-31 16:44:57 -0800

    Added player smooth turning

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -11,7 +11,12 @@ namespace Player
         [Header("References")]
         [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private SpriteRenderer _maskSpriteRenderer;
-        [Header("Config")] [SerializeField] private float _walkSpeed;
+        [Header("Config")] 
+        [SerializeField] private float _walkSpeed;
+        [SerializeField] private float _turnSpeedLimit = 1f;
+        [SerializeField] private float _turnThreshold = 5f;
+
+        private Vector2 _movementVector = Vector2.right;
 
         private void Start()
         {
@@ -22,8 +27,13 @@ namespace Player
         private void MovePlayer()
         {
             var rawInput = InputManager.Instance.playerInput.Player.Move.ReadValue<Vector2>();
-            Vector2 movementVector = rawInput.magnitude > 0.95f ? rawInput : Vector2.zero;
-            _rigidbody2D.MovePosition(_rigidbody2D.position + movementVector * (_walkSpeed * Time.fixedDeltaTime));
+            _movementVector = rawInput.magnitude > 0.95f ? rawInput : Vector2.zero;
+            _rigidbody2D.MovePosition(_rigidbody2D.position + _movementVector * (_walkSpeed * Time.fixedDeltaTime));
+        }
+
+        private void Update()
+        {
+            AlignTowardMovement(Time.deltaTime);
         }
 
         private void FixedUpdate()
@@ -35,6 +45,33 @@ namespace Player
         {
             _maskSpriteRenderer.enabled = newColor != MaskColor.Default;
             _maskSpriteRenderer.color = newColor.GetColor();
+        }
+
+        /// <summary>
+        /// Aligns the player's rotation toward the direction its moving, with alignment speed capped
+        /// by '_turnSpeedLimit'
+        /// </summary>
+        private void AlignTowardMovement(float deltaTime)
+        {
+            if (_movementVector.magnitude < 0.5f) return;
+            
+            Vector3 curRot = transform.eulerAngles;
+            curRot.z -= 90f;  // Sprite rotation offset
+            Debug.Log($"Cur angle: {curRot.z}");
+            
+            float angleFrom = curRot.z;
+            float angleTo = Mathf.Atan2(_movementVector.y, _movementVector.x) * Mathf.Rad2Deg;
+            Debug.Log($"Target angle: {angleTo}");
+            float deltaAngle = Mathf.DeltaAngle(angleFrom, angleTo);
+            Debug.Log($"Rotating by: {deltaAngle}");
+            if (Mathf.Abs(deltaAngle) < _turnThreshold) return;
+            float frameTurnLimit = _turnSpeedLimit * deltaTime;
+            if (frameTurnLimit < Mathf.Abs(deltaAngle)) deltaAngle = Mathf.Sign(deltaAngle) * frameTurnLimit;
+
+            curRot.z += deltaAngle;
+            curRot.z += 90f;  // Undo sprite rotation offset
+            Debug.Log($"New rot {curRot.z}");
+            transform.eulerAngles = curRot;
         }
     }
 }
