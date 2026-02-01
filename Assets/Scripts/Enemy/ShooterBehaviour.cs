03c7107740a6cbf0ca0a3664b5de5c8724bdea2a
nathanrun1 <nathanmreilly@gmail.com>
2026-01-31 21:13:06 -0800

    feat: cosmetic shooter ray

--- Assets/Scripts/Enemy/ShooterBehaviour.cs
+++ Assets/Scripts/Enemy/ShooterBehaviour.cs
@@ -9,11 +9,17 @@ namespace Enemy
     public class ShooterBehaviour : EnemyBehaviour
     {  
         private Vector2 _curMovementDirection;
+        
+        [Header("References")]
+        [SerializeField] LineRenderer _lineRenderer;
+        [SerializeField] Transform _shotOrigin;
 
         protected override void OnValidate()
         {
             base.OnValidate();
             AlignWithMonitorDirection();
+            _lineRenderer.startColor = _maskColor.GetColor();
+            _lineRenderer.endColor = _maskColor.GetColor();
         }
 
         private void FixedUpdate()
@@ -26,9 +32,26 @@ namespace Enemy
         /// </summary>
         private void TryHitPlayer()
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, _monitorDirection.ToVector2(), 
+            Vector2 shotOrigin = _shotOrigin.position;
+            Vector2 shotDir = _monitorDirection.ToVector2();
+            
+            RaycastHit2D hit = Physics2D.Raycast(shotOrigin, shotDir, 
                 float.MaxValue, (int)CollisionAssistant.VisibleToEnemy);
             if (hit.collider.gameObject == LevelManager.Instance.playerObject) GameManager.PlayerLoss();
+
+            DisplayShotLine(shotOrigin, shotDir, hit);
+        }
+
+        /// <summary>
+        /// Displays a visible "shot line" along the shooter's line of sight
+        /// </summary>
+        private void DisplayShotLine(Vector3 shotOrigin, Vector2 shotDir, RaycastHit2D hit)
+        {
+            _lineRenderer.SetPosition(0, shotOrigin);
+            if (hit)
+                _lineRenderer.SetPosition(1, hit.point);
+            else
+                _lineRenderer.SetPosition(1, shotOrigin + (Vector3)shotDir * 100f);
         }
         
         /// <summary>
